14a1e53a24216b2073d05da13d5ac4509297af33
Arjuna Kankipati <ajrk001@bucknell.edu>
2013-11-15 10:44:57 -0500

    Began work on the GUI

---
+++ src/gui/MBoardPanel.java
@@ -0,0 +1,16 @@
+/**
+ * CSCI 205 - Software Design and Engineering
+ * Name: Arjuna Kankipati
+ * Semester: Fall 2013
+ * Work: FinalProject
+ * Created: Nov 15, 2013, 10:13:56 AM
+ */
+package gui;
+
+/**
+ * @author ajrk001
+ *
+ */
+public class MBoardPanel {
+
+}
---
+++ src/gui/MMainFrame.java
@@ -0,0 +1,16 @@
+/**
+ * CSCI 205 - Software Design and Engineering
+ * Name: Arjuna Kankipati
+ * Semester: Fall 2013
+ * Work: FinalProject
+ * Created: Nov 15, 2013, 10:13:41 AM
+ */
+package gui;
+
+/**
+ * @author ajrk001
+ *
+ */
+public class MMainFrame {
+
+}
