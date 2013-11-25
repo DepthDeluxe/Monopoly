63e96666fdf5ef42e1b7ede4ae26911fe63cc908
DepthDeluxe <cheinzmann3@gmail.com>
2013-11-25 15:24:14 -0500

    added a property list for player and member funcs added support for buying properties, checking to see if the player owns a property, and getting the number of railroads a player owns (for calculating railroad rent)

--- src/monopoly/Player.java
+++ src/monopoly/Player.java
@@ -1,5 +1,7 @@
 package monopoly;
 
+import java.util.LinkedList;
+
 public class Player {
 	//
 	// Member Variables
@@ -8,6 +10,8 @@ public class Player {
 	private int position;
 	private double money;
 	
+	private LinkedList<Property> properties;
+	
 	//
 	// Constructors
 	//
@@ -15,6 +19,8 @@ public class Player {
 	public Player(int startMoney) {
 		this.money = startMoney;
 		position = 0;
+		
+		properties = new LinkedList<Property>();
 	}
 	
 	//
@@ -34,15 +40,62 @@ public class Player {
 		this.money += amount;
 	}
 	
-	public void takeMoney(double amount) {
+	public boolean takeMoney(double amount) {
+		// don't allow player to have negative money
+		if (money < amount) {
+			return false;
+		}
+		
+		// subtract the money and return true
 		this.money -= amount;
+		return true;
 	}
 
+	public boolean buyProperty(Property p) {
+		// can't buy if not enough money
+		double price = p.getPrice();
+		if (money < price) {
+			return false;
+		}
+		// can't buy if property already owned
+		if (p.getOwner() != null) {
+			return false;
+		}
+		
+		// subtract to price and add to owner list
+		money -= price;
+		properties.add(p);
+		p.setOwner(this);		// set the new owner of property
+		return true;
+	}
+	
 	//
 	// Getters
 	//
 	
 	public double getCurrentMoney() {
 		return money;
+	}
+	
+	public boolean owns(Property p) {
+		return properties.contains(p);
+	}
+	
+	public int getNumRailroads() {
+		// iterate through the properties, seeing which ones you
+		// can typecast to Railroad.  If it can't, it will throw
+		// a ClassCastException
+		int numRailroads = 0;
+		for (Property p : properties) {
+			try {
+				Railroad r = (Railroad)p;
+				numRailroads++;
+			}
+			catch (ClassCastException e) {
+				// don't do anything, this is not a railroad
+			}
+		}
+		
+		return numRailroads;
 	}
 }
