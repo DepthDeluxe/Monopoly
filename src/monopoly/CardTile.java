fcb827ebf71d2b7af6093732a157acca57a7db71
DepthDeluxe <cheinzmann3@gmail.com>
2013-11-29 19:47:19 -0500

    added CardTile which manages chance and com chest All it does is return the appropriate flag so the controller can manage it.

---
+++ src/monopoly/CardTile.java
@@ -0,0 +1,37 @@
+package monopoly;
+
+enum CardType {
+	CHANCE, COMMUNITY_CHEST;
+}
+
+public class CardTile implements ITile {
+	//
+	// Member Variables
+	//
+	
+	CardType cardType;
+	
+	//
+	// Constructors
+	//
+	
+	public CardTile(CardType cardType) {
+		this.cardType = cardType;
+	}
+	
+	//
+	// ITile Implementation
+	//
+
+	@Override
+	public MonopolyModelState landOn(Player p) {
+		// send appropriate flag to the controller to let it
+		// know that a chance or community chest card must be drawn
+		if (cardType == CardType.CHANCE) {
+			return MonopolyModelState.CHANCE;
+		}
+		else {
+			return MonopolyModelState.COMMUNITY_CHEST;
+		}
+	}
+}
