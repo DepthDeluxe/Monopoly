f15956b69c259c9410bebc356f68c880a6747d63
DepthDeluxe <cheinzmann3@gmail.com>
2013-11-29 19:41:24 -0500

    added FreeParking class special behavior required for free parking.  The board should have a direct reference to the free parking tile so all money that people have to pay from chance cards goes into that pile

---
+++ src/monopoly/FreeParking.java
@@ -0,0 +1,43 @@
+package monopoly;
+
+public class FreeParking implements ITile {
+	//
+	// Member Variables
+	//
+	
+	private double moneyInPot;
+	private double nextPotStart;
+	
+	//
+	// Constructors
+	//
+	
+	public FreeParking(double amountToStart) {
+		this.moneyInPot = amountToStart;
+		this.nextPotStart = 2 * this.moneyInPot;	// amount in pot doubles every time
+	}
+	
+	//
+	// Main Methods
+	//
+	
+	public void addToPot(double amount) {
+		moneyInPot += amount;
+	}
+	
+	//
+	// ITile implementation
+	//
+	
+	@Override
+	public MonopolyModelState landOn(Player p) {
+		// give player the money that is in the pot
+		p.giveMoney(moneyInPot);
+		
+		// fill the pot with some money, 
+		moneyInPot = nextPotStart;
+		nextPotStart *= 2;
+		
+		return MonopolyModelState.PLAYING;
+	}
+}
