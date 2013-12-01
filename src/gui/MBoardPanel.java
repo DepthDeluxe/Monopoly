9b8b479ed4cc37260fa65db52c567518b2437293
Arjuna Kankipati <ajrk001@bucknell.edu>
2013-12-01 11:17:52 -0500

    Working on getting individual boxes

--- src/gui/MBoardPanel.java
+++ src/gui/MBoardPanel.java
@@ -17,6 +17,8 @@ import javax.swing.UIManager;
 import javax.swing.border.LineBorder;
 import java.awt.Font;
 import java.awt.Component;
+import javax.swing.ImageIcon;
+import javax.swing.JTextArea;
 
 /**
  * @author ajrk001
@@ -39,222 +41,250 @@ public class MBoardPanel extends JPanel
 	{
 		this.positions = new int[40];
 		this.players = new JLabel[4];
-		this.setSize(500, 500);
+		this.setSize(650, 650);
 		setLayout(null);
 		
 		Box startBox = Box.createVerticalBox();
 		startBox.setBorder(new LineBorder(new Color(0, 0, 0)));
 		startBox.setBackground(Color.BLACK);
-		startBox.setBounds(421, 421, 79, 79);
+		startBox.setBounds(550, 550, 100, 100);
 		add(startBox);
 		
 		Box property1 = Box.createVerticalBox();
 		property1.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property1.setBounds(383, 421, 38, 79);
+		property1.setBounds(500, 550, 50, 100);
 		add(property1);
+		
+		JLabel colorBox1 = new JLabel("");
+		colorBox1.setIcon(new ImageIcon(MBoardPanel.class.getResource("/gui/purple.jpg")));
+		property1.add(colorBox1);
 		
 		Box property2 = Box.createVerticalBox();
 		property2.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property2.setBounds(345, 421, 38, 79);
+		property2.setBounds(450, 550, 50, 100);
 		add(property2);
 		
 		Box property3 = Box.createVerticalBox();
 		property3.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property3.setBounds(307, 421, 38, 79);
+		property3.setBounds(400, 550, 50, 100);
 		add(property3);
+		
+		JLabel colorBox2 = new JLabel("");
+		colorBox2.setIcon(new ImageIcon(MBoardPanel.class.getResource("/gui/purple.jpg")));
+		property3.add(colorBox2);
 		
 		Box property4 = Box.createVerticalBox();
 		property4.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property4.setBounds(269, 421, 38, 79);
+		property4.setBounds(350, 550, 50, 100);
 		add(property4);
 		
 		Box property5 = Box.createVerticalBox();
 		property5.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property5.setBounds(231, 421, 38, 79);
+		property5.setBounds(300, 550, 50, 100);
 		add(property5);
 		
 		Box property6 = Box.createVerticalBox();
 		property6.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property6.setBounds(193, 421, 38, 79);
+		property6.setBounds(250, 550, 50, 100);
 		add(property6);
+		
+		JLabel colorBox3 = new JLabel("");
+		colorBox3.setIcon(new ImageIcon(MBoardPanel.class.getResource("/gui/blue.jpg")));
+		property6.add(colorBox3);
 		
 		Box property7 = Box.createVerticalBox();
 		property7.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property7.setBounds(155, 421, 38, 79);
+		property7.setBounds(200, 550, 50, 100);
 		add(property7);
 		
 		Box property8 = Box.createVerticalBox();
 		property8.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property8.setBounds(117, 421, 38, 79);
+		property8.setBounds(150, 550, 50, 100);
 		add(property8);
+		
+		JLabel colorBox4 = new JLabel("");
+		colorBox4.setIcon(new ImageIcon(MBoardPanel.class.getResource("/gui/blue.jpg")));
+		property8.add(colorBox4);
 		
 		Box property9 = Box.createVerticalBox();
 		property9.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property9.setBounds(79, 421, 38, 79);
+		property9.setBounds(100, 550, 50, 100);
 		add(property9);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon(MBoardPanel.class.getResource("/gui/blue.jpg")));
+		property9.add(label_1);
 		
 		Box jailBox = Box.createVerticalBox();
 		jailBox.setBorder(new LineBorder(new Color(0, 0, 0)));
-		jailBox.setBounds(0, 421, 79, 79);
+		jailBox.setBounds(0, 550, 100, 100);
 		add(jailBox);
 		
 		Box property10 = Box.createHorizontalBox();
 		property10.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property10.setBounds(0, 383, 79, 38);
+		property10.setBounds(0, 500, 100, 50);
 		add(property10);
+		
+		JTextArea txtrProperty = new JTextArea();
+		txtrProperty.setText("Property");
+		property10.add(txtrProperty);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(MBoardPanel.class.getResource("/gui/magenta.jpg")));
+		property10.add(lblNewLabel_1);
 		
 		Box property11 = Box.createHorizontalBox();
 		property11.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property11.setBounds(0, 345, 79, 38);
+		property11.setBounds(0, 450, 100, 50);
 		add(property11);
 		
 		Box property12 = Box.createHorizontalBox();
 		property12.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property12.setBounds(0, 307, 79, 38);
+		property12.setBounds(0, 400, 100, 50);
 		add(property12);
 		
 		Box property13 = Box.createHorizontalBox();
 		property13.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property13.setBounds(0, 269, 79, 38);
+		property13.setBounds(0, 350, 100, 50);
 		add(property13);
 		
 		Box property14 = Box.createHorizontalBox();
 		property14.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property14.setBounds(0, 231, 79, 38);
+		property14.setBounds(0, 300, 100, 50);
 		add(property14);
 		
 		Box property15 = Box.createHorizontalBox();
 		property15.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property15.setBounds(0, 193, 79, 38);
+		property15.setBounds(0, 250, 100, 50);
 		add(property15);
 		
 		Box property16 = Box.createHorizontalBox();
 		property16.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property16.setBounds(0, 155, 79, 38);
+		property16.setBounds(0, 200, 100, 50);
 		add(property16);
 		
 		Box property17 = Box.createHorizontalBox();
 		property17.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property17.setBounds(0, 117, 79, 38);
+		property17.setBounds(0, 150, 100, 50);
 		add(property17);
 		
 		Box property18 = Box.createHorizontalBox();
 		property18.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property18.setBounds(0, 79, 79, 38);
+		property18.setBounds(0, 100, 100, 50);
 		add(property18);
 		
 		Box freeParking = Box.createHorizontalBox();
 		freeParking.setBorder(new LineBorder(new Color(0, 0, 0)));
-		freeParking.setBounds(0, 0, 79, 79);
+		freeParking.setBounds(0, 0, 100, 100);
 		add(freeParking);
 		
 		Box property19 = Box.createVerticalBox();
 		property19.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property19.setBounds(79, 0, 38, 79);
+		property19.setBounds(100, 0, 50, 100);
 		add(property19);
 		
 		Box property20 = Box.createHorizontalBox();
 		property20.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property20.setBounds(117, 0, 38, 79);
+		property20.setBounds(150, 0, 50, 100);
 		add(property20);
 		
 		Box property21 = Box.createHorizontalBox();
 		property21.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property21.setBounds(155, 0, 38, 79);
+		property21.setBounds(200, 0, 50, 100);
 		add(property21);
 		
 		Box property22 = Box.createHorizontalBox();
 		property22.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property22.setBounds(193, 0, 38, 79);
+		property22.setBounds(250, 0, 50, 100);
 		add(property22);
 		
 		Box property23 = Box.createHorizontalBox();
 		property23.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property23.setBounds(231, 0, 38, 79);
+		property23.setBounds(300, 0, 50, 100);
 		add(property23);
 		
 		Box property24 = Box.createHorizontalBox();
 		property24.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property24.setBounds(269, 0, 38, 79);
+		property24.setBounds(350, 0, 50, 100);
 		add(property24);
 		
 		Box property25 = Box.createHorizontalBox();
 		property25.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property25.setBounds(307, 0, 38, 79);
+		property25.setBounds(400, 0, 50, 100);
 		add(property25);
 		
 		Box property26 = Box.createHorizontalBox();
 		property26.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property26.setBounds(345, 0, 38, 79);
+		property26.setBounds(450, 0, 50, 100);
 		add(property26);
 		
 		Box property27 = Box.createHorizontalBox();
 		property27.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property27.setBounds(383, 0, 38, 79);
+		property27.setBounds(500, 0, 50, 100);
 		add(property27);
 		
 		Box goToJail = Box.createHorizontalBox();
 		goToJail.setBorder(new LineBorder(new Color(0, 0, 0)));
-		goToJail.setBounds(421, 0, 79, 79);
+		goToJail.setBounds(550, 0, 100, 100);
 		add(goToJail);
 		
 		Box property28 = Box.createHorizontalBox();
 		property28.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property28.setBounds(421, 79, 79, 38);
+		property28.setBounds(550, 100, 100, 50);
 		add(property28);
 		
 		Box property29 = Box.createHorizontalBox();
 		property29.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property29.setBounds(421, 117, 79, 38);
+		property29.setBounds(550, 150, 100, 50);
 		add(property29);
 		
 		Box property30 = Box.createHorizontalBox();
 		property30.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property30.setBounds(421, 155, 79, 38);
+		property30.setBounds(550, 200, 100, 50);
 		add(property30);
 		
 		Box property31 = Box.createHorizontalBox();
 		property31.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property31.setBounds(421, 193, 79, 38);
+		property31.setBounds(550, 250, 100, 50);
 		add(property31);
 		
 		Box property32 = Box.createHorizontalBox();
 		property32.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property32.setBounds(421, 231, 79, 38);
+		property32.setBounds(550, 300, 100, 50);
 		add(property32);
 		
 		Box property33 = Box.createHorizontalBox();
 		property33.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property33.setBounds(421, 269, 79, 38);
+		property33.setBounds(550, 350, 100, 50);
 		add(property33);
 		
 		Box property34 = Box.createHorizontalBox();
 		property34.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property34.setBounds(421, 307, 79, 38);
+		property34.setBounds(550, 400, 100, 50);
 		add(property34);
 		
 		Box property35 = Box.createHorizontalBox();
 		property35.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property35.setBounds(421, 345, 79, 38);
+		property35.setBounds(550, 450, 100, 50);
 		add(property35);
 		
 		Box property36 = Box.createHorizontalBox();
 		property36.setBorder(new LineBorder(new Color(0, 0, 0)));
-		property36.setBounds(421, 383, 79, 38);
+		property36.setBounds(550, 500, 100, 50);
 		add(property36);
 		
 		JLabel lblMonopoly = new JLabel("MONOPOLY");
 		lblMonopoly.setFont(new Font("Manteka", Font.BOLD, 34));
-		lblMonopoly.setBounds(154, 204, 191, 52);
+		lblMonopoly.setBounds(241, 275, 191, 52);
 		add(lblMonopoly);
 		
 		JLabel lblRedditEdition = new JLabel("Reddit Edition");
-		lblRedditEdition.setBounds(219, 255, 66, 14);
+		lblRedditEdition.setBounds(300, 331, 66, 14);
 		add(lblRedditEdition);
 		
 		Box chanceBox = Box.createHorizontalBox();
 		chanceBox.setBorder(new LineBorder(new Color(0, 0, 0)));
-		chanceBox.setBounds(294, 323, 89, 58);
+		chanceBox.setBounds(411, 442, 89, 58);
 		add(chanceBox);
 		
 		JLabel lblChance = new JLabel("Chance");
@@ -262,7 +292,7 @@ public class MBoardPanel extends JPanel
 		
 		Box ccBox = Box.createHorizontalBox();
 		ccBox.setBorder(new LineBorder(new Color(0, 0, 0)));
-		ccBox.setBounds(117, 106, 89, 58);
+		ccBox.setBounds(150, 150, 89, 58);
 		add(ccBox);
 		
 		JLabel lblNewLabel = new JLabel("Community Chest");
