60e1c60045c0daf284f55827aac62dd781b56f7e
DepthDeluxe <cheinzmann3@gmail.com>
2013-12-05 23:53:16 -0500

    implemented basic view updating from model only when the player moves...

--- src/controller/RollDiceAction.java
+++ src/controller/RollDiceAction.java
@@ -36,7 +36,7 @@ public class RollDiceAction implements ActionListener {
 	 * @return TRUE if the user wants to buy
 	 */
 	private boolean handleBuyRequest() {
-		
+		return false;
 	}
 	
 	private void handleChance() {
@@ -60,6 +60,10 @@ public class RollDiceAction implements ActionListener {
 		Dice d = theGame.getDice();
 		theBoardView.rollDice(d);
 		
+		// update the current player position
+		Player currentPlayer = theGame.getCurrentPlayer();
+		theBoardView.moveCharacter(theGame.getCurrentPlayerIndex(), currentPlayer.getPosition());
+		
 		// run the right function depending on the state of the model
 		switch(theGame.getModelState()) {
 		case BUY_REQUEST:
@@ -72,6 +76,7 @@ public class RollDiceAction implements ActionListener {
 			
 		case COMMUNITY_CHEST:
 			handleCommChest();
+			break;
 		
 		// PLAYING state
 		default:
