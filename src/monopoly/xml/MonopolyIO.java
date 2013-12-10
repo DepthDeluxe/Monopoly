e5c543fc9ba75a17a997ab2cf6beecf80a45b92c
DepthDeluxe <cheinzmann3@gmail.com>
2013-12-10 13:49:16 -0500

    laid framework for saving serialized xml

---
+++ src/monopoly/xml/MonopolyIO.java
@@ -0,0 +1,14 @@
+package monopoly.xml;
+
+import monopoly.Monopoly;
+
+public class MonopolyIO {
+	public static void saveGame(String filename, Monopoly theGame) {
+		// always overwrite the file
+		throw new RuntimeException("Saving not implemented!");
+	}
+	
+	public static Monopoly loadGame(String filename) {
+		throw new RuntimeException("Loading not implemented!");
+	}
+}
